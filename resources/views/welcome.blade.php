1cc1b7d5b8677f56b7adf3c3042caa26030dde13
ffrahman <fatur14022@gmail.com>
2022-03-27 17:08:07 +0700

    welcome,login, route

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,464 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Go Wiss | Login</title>
+
+    <!-- Font Awesome -->
+    <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+    rel="stylesheet"
+    />
+    <!-- Google Fonts -->
+    <link
+    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    rel="stylesheet"
+    />
+    <!-- MDB -->
+    <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.11.0/mdb.min.css"
+    rel="stylesheet"
+    />
+</head>
+<body>
+        <!--Main Navigation-->
+    <header>
+    <style>
+        /* Carousel styling */
+        #introCarousel,
+        .carousel-inner,
+        .carousel-item,
+        .carousel-item.active {
+        height: 100vh;
+        }
+
+        .carousel-item:nth-child(1) {
+        background-image: url('https://mdbootstrap.com/img/Photos/Others/images/76.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center center;
+        }
+        .carousel-item:nth-child(2) {
+        background-image: url('https://mdbootstrap.com/img/Photos/Others/images/77.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center center;
+        }
+        .carousel-item:nth-child(3) {
+        background-image: url('https://mdbootstrap.com/img/Photos/Others/images/78.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center center;
+        }
+
+        /* Height for devices larger than 576px */
+        @media (min-width: 992px) {
+        #introCarousel {
+            margin-top: -58.59px;
+        }
+        #introCarousel,
+        .carousel-inner,
+        .carousel-item,
+        .carousel-item.active {
+            height: 80vh;
+        }
+        }
+
+        .navbar .nav-link {
+        color: #fff !important;
+        }
+    </style>
+
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark d-none d-lg-block" style="z-index: 2000;">
+        <div class="container-fluid">
+        <!-- Navbar brand -->
+        <a class="navbar-brand nav-link" target="_blank" href="#">
+            <strong>Go Wiss</strong>
+        </a>
+        <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarExample01"
+            aria-controls="navbarExample01" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarExample01">
+            <ul class="navbar-nav ms-auto mb-3 mb-lg-0">
+            <li class="nav-item active">
+                <a class="nav-link" aria-current="page" href="/home">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" rel="nofollow"
+                target="_blank">Melihat Wisata</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" target="_blank">Paket Wisata</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" target="_blank">Sewa Kendaraan</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" target="_blank">Berita</a>
+            </li>
+            </ul>
+        </div>
+        </div>
+    </nav>
+    <!-- Navbar -->
+
+    <!-- Carousel wrapper -->
+    <div id="introCarousel" class="carousel slide carousel-fade shadow-2-strong" data-mdb-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+        <li data-mdb-target="#introCarousel" data-mdb-slide-to="0" class="active"></li>
+        <li data-mdb-target="#introCarousel" data-mdb-slide-to="1"></li>
+        <li data-mdb-target="#introCarousel" data-mdb-slide-to="2"></li>
+        </ol>
+
+        <!-- Inner -->
+        <div class="carousel-inner">
+        <!-- Single item -->
+        <div class="carousel-item active">
+            <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+            <div class="d-flex justify-content-center align-items-center h-100">
+                <div class="text-white text-center">
+                <h1 class="mb-3">Healing</h1>
+                <h5 class="mb-4">skuyy</h5>
+                <a
+                    class="btn btn-outline-light btn-lg m-2"
+                    href="#"
+                    role="button"
+                    rel="nofollow"
+                    target="_blank"
+                    >Lihat Wisata</a
+                >
+                <a
+                    class="btn btn-outline-light btn-lg m-2"
+                    href="#"
+                    target="_blank"
+                    role="button"
+                    >Beli Paket</a
+                >
+                </div>
+            </div>
+            </div>
+        </div>
+
+        <!-- Single item -->
+        <div class="carousel-item">
+            <div class="mask" style="background-color: rgba(0, 0, 0, 0.3);">
+            <div class="d-flex justify-content-center align-items-center h-100">
+                <div class="text-white text-center">
+                <h2>You can place here any content</h2>
+                </div>
+            </div>
+            </div>
+        </div>
+
+        <!-- Single item -->
+        <div class="carousel-item">
+            <div
+            class="mask"
+            style="
+                background: linear-gradient(
+                45deg,
+                rgba(29, 236, 197, 0.7),
+                rgba(91, 14, 214, 0.7) 100%
+                );
+            "
+            >
+            <div class="d-flex justify-content-center align-items-center h-100">
+                <div class="text-white text-center">
+                <h2>And cover it with any mask</h2>
+                <a
+                    class="btn btn-outline-light btn-lg m-2"
+                    href="#"
+                    target="_blank"
+                    role="button"
+                    >Learn  </a
+                >
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+        <!-- Inner -->
+
+        <!-- Controls -->
+        <a class="carousel-control-prev" href="#introCarousel" role="button" data-mdb-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#introCarousel" role="button" data-mdb-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <!-- Carousel wrapper -->
+    </header>
+    <!--Main Navigation-->
+
+    <!--Main layout-->
+    <main class="mt-5">
+    <div class="container">
+        <!--Section: Content-->
+        <section>
+        <div class="row">
+            <div class="col-md-6 gx-5 mb-4">
+            <div class="bg-image hover-overlay ripple shadow-2-strong rounded-5" data-mdb-ripple-color="light">
+                <img src="{{ asset('img/lembang-zoo.jpg') }}" class="img-fluid" />
+                <a href="#!">
+                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+            </div>
+            </div>
+
+            <div class="col-md-6 gx-5 mb-4">
+            <h4><strong>Lembang PARK & ZOO</strong></h4>
+            <p class="text-muted">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis consequatur
+                eligendi quisquam doloremque vero ex debitis veritatis placeat unde animi laborum
+                sapiente illo possimus, commodi dignissimos obcaecati illum maiores corporis.
+            </p>
+            <p><strong>...</strong></p>
+            <p class="text-muted">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod itaque voluptate
+                nesciunt laborum incidunt. Officia, quam consectetur. Earum eligendi aliquam illum
+                alias, unde optio accusantium soluta, iusto molestiae adipisci et?
+            </p>
+            </div>
+        </div>
+        </section>
+        <!--Section: Content-->
+
+        <hr class="my-5" />
+
+        <!--Section: Content-->
+        <section class="text-center">
+        <h4 class="mb-5"><strong>Destinasi Wisata Lembang</strong></h4>
+
+        <div class="row">
+            <div class="col-lg-4 col-md-12 mb-4">
+            <div class="card">
+                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                <img
+                    src="{{ asset('img/begonia.jpg') }}"
+                    class="img-fluid"
+                />
+                <a href="#!">
+                    <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+                </div>
+                <div class="card-body">
+                <h5 class="card-title">Taman Begonia</h5>
+                <p class="card-text">
+                    Tak hanya keindahan bunga yang menarik, taman ini juga memiliki daya tarik pada tata letak taman yang sangat menarik.
+                </p>
+                <a href="#!" class="btn btn-primary">Lihat Wisata</a>
+                </div>
+            </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card">
+                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                <img
+                    src="{{ asset('img/cikole.jpg') }}"
+                    class="img-fluid"
+                />
+                <a href="#!">
+                    <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+                </div>
+                <div class="card-body">
+                <h5 class="card-title">Grafika Cikole Lembang</h5>
+                <p class="card-text">
+                    Tempat wisata Lembang ini menjadi salah satu obyek wisata yang baru dan sangat instagramable. Moms bisa melakukan foto bersama dengan rumah hobbit yang hits di sosial media.
+                </p>
+                <a href="#!" class="btn btn-primary">Lihat Wisata</a>
+                </div>
+            </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card">
+                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                <img
+                    src="{{ asset('img/kawah-putih.jpg') }}"
+                    class="img-fluid"
+                />
+                <a href="#!">
+                    <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+                </div>
+                <div class="card-body">
+                <h5 class="card-title">Kawah Putih Ciwidey</h5>
+                <p class="card-text">
+                    Kawah Putih Ciwidey juga jadi salah satu rekomendasi tempat wisata Lembang terfavorit.
+                </p>
+                <a href="#!" class="btn btn-primary">Lihat Wisata</a>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+        <!--Section: Content-->
+
+        <hr class="my-5" />
+
+        <!--Section: Content-->
+        <section class="mb-5">
+        <h4 class="mb-5 text-center"><strong>Sign into your account</strong></h4>
+
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-6">
+                <!-- Pills navs -->
+                <ul class="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
+                    <li class="nav-item" role="presentation">
+                    <a
+                        class="nav-link active"
+                        id="tab-login"
+                        data-mdb-toggle="pill"
+                        href="#pills-login"
+                        role="tab"
+                        aria-controls="pills-login"
+                        aria-selected="true"
+                        >Login</a
+                    >
+                    </li>
+                    <li class="nav-item" role="presentation">
+                    <a
+                        class="nav-link"
+                        id="tab-register"
+                        data-mdb-toggle="pill"
+                        href="#pills-register"
+                        role="tab"
+                        aria-controls="pills-register"
+                        aria-selected="false"
+                        >Register</a
+                    >
+                    </li>
+                </ul>
+  <!-- Pills navs -->
+  
+  <!-- Pills content -->
+  <div class="tab-content">
+    <div class="tab-pane fade show active" id="pills-login" role="tabpanel" aria-labelledby="tab-login">
+      <form>
+        <!-- Email input -->
+        <div class="form-outline mb-4">
+          <input type="email" id="loginName" class="form-control" />
+          <label class="form-label" for="loginName">Email or username</label>
+        </div>
+  
+        <!-- Password input -->
+        <div class="form-outline mb-4">
+          <input type="password" id="loginPassword" class="form-control" />
+          <label class="form-label" for="loginPassword">Password</label>
+        </div>
+  
+        <!-- 2 column grid layout -->
+        <div class="row mb-4">
+          <div class="col-md-6 d-flex justify-content-center">
+            <!-- Checkbox -->
+            <div class="form-check mb-3 mb-md-0">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                value=""
+                id="loginCheck"
+                checked
+              />
+              <label class="form-check-label" for="loginCheck"> Remember me </label>
+            </div>
+          </div>
+  
+          <div class="col-md-6 d-flex justify-content-center">
+            <!-- Simple link -->
+            <a href="#!">Forgot password?</a>
+          </div>
+        </div>
+  
+        <!-- Submit button -->
+        <button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
+  
+        <!-- Register buttons -->
+      </form>
+    </div>
+    <div
+      class="tab-pane fade"
+      id="pills-register"
+      role="tabpanel"
+      aria-labelledby="tab-register"
+    >
+      <form>
+        <!-- Name input -->
+        <div class="form-outline mb-4">
+          <input type="text" id="registerName" class="form-control" />
+          <label class="form-label" for="registerName">Name</label>
+        </div>
+  
+        <!-- Username input -->
+        <div class="form-outline mb-4">
+          <input type="text" id="registerUsername" class="form-control" />
+          <label class="form-label" for="registerUsername">Username</label>
+        </div>
+  
+        <!-- Email input -->
+        <div class="form-outline mb-4">
+          <input type="email" id="registerEmail" class="form-control" />
+          <label class="form-label" for="registerEmail">Email</label>
+        </div>
+  
+        <!-- Password input -->
+        <div class="form-outline mb-4">
+          <input type="password" id="registerPassword" class="form-control" />
+          <label class="form-label" for="registerPassword">Password</label>
+        </div>
+  
+        <!-- Repeat Password input -->
+        <div class="form-outline mb-4">
+          <input type="password" id="registerRepeatPassword" class="form-control" />
+          <label class="form-label" for="registerRepeatPassword">Repeat password</label>
+        </div>
+  
+        <!-- Checkbox -->
+        <div class="form-check d-flex justify-content-center mb-4">
+          <input
+            class="form-check-input me-2"
+            type="checkbox"
+            value=""
+            id="registerCheck"
+            checked
+            aria-describedby="registerCheckHelpText"
+          />
+          <label class="form-check-label" for="registerCheck">
+            I have read and agree to the terms
+          </label>
+        </div>
+  
+        <!-- Submit button -->
+        <button type="submit" class="btn btn-primary btn-block mb-3">Register</button>
+      </form>
+    </div>  
+  </div>
+</div>
+</div>
+<!-- Pills content -->
+
+<!-- Copyright -->
+<div>
+    <div class="text-center p-3" style=" background-color: rgba(0, 0, 0, 0.2); " >
+        <a class="text-dark" href="#"></a>
+    </div>
+</div>
+<!-- Copyright -->
+</footer>
+<!--Footer-->
+<!-- MDB -->
+<script
+type="text/javascript"
+src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.11.0/mdb.min.js"
+></script>
+</body>
+</html>
