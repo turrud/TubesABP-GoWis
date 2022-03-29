df9bf7921babc78b9a856ab41f2585699536ba6a
ffrahman <fatur14022@gmail.com>
2022-03-29 08:13:59 +0700

    change img

--- resources/views/PaketWisata.blade.php
+++ resources/views/PaketWisata.blade.php
@@ -98,11 +98,11 @@
           <!-- Three columns of text below the carousel -->
           <div class="row">
             <div class="col-lg-4">
-              <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg"
+              <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="{{ asset('img/gowis.jpg') }}"
                 role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title><img src="{{ asset('img/ciater.jpg') }}" alt=""></title>
-                <rect width="100%" height="100%" fill="#77 7" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
-              </svg>
+                <title></title>
+                <rect width="100%" height="100%" fill="#77 7" /><text x="50%" y="50%" fill="#777" dy=".3em"></text>
+            </img>
     
               <h2>Heading</h2>
               <p>Some representative placeholder content for the three columns of text below the carousel. This is the first
